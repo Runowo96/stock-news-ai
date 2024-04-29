54c2211789cc65ee41c1978a548029d4bdc15e96
runowo96 <devinmrunowski@gmail.com>
2024-04-29 13:25:56 -0400

    added post request and form to Signup.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       			<BrowserRouter>
 				<Routes>
 					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
 				</Routes>
 			</BrowserRouter>
     </div>
